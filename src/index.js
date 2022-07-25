b19680aa02ce99ebea0a54f635b378dd1451e81c
raquelthabata <r.thabata05@gmail.com>
2022-07-25 12:51:04 -0300

    animação do parágrafo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import validator from './validator.js';
-
-console.log(validator);
-
-const botao= document.getElementById("validador")
-botao.addEventListener("click",function(event){
-    console.log(event)
-})
-
 function typeWriter(e) {
     const textoArray = e.innerHTML.split('')
     e.innerHTML = ''
-    textoArray.forEach ((letra,i) => { setTimeout (() => e.innerHTML += letra, 75 * i )});
-
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => e.innerHTML += letra, 75 * i)
+    });
 }
-
-const texto = document.querySelector ('p')
-typeWriter (texto)
+const texto = document.querySelector('p')
+typeWriter(texto)
