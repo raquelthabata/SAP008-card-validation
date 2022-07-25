4d1c5a63821f5237e0e6c6922440f6d141028e47
raquelthabata <r.thabata05@gmail.com>
2022-07-25 15:48:01 -0300

    "Pegar o valor de input com JS"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import validator from './validator.js';
+
+console.log(validator);
+
+
 function typeWriter(e) {
     const textoArray = e.innerHTML.split('')
     e.innerHTML = ''
@@ -5,5 +10,13 @@ function typeWriter(e) {
         setTimeout(() => e.innerHTML += letra, 75 * i)
     });
 }
-const texto = document.querySelector('p')
-typeWriter(texto)
+const palavras = document.querySelector('p')
+typeWriter(palavras)
+
+const pegarvalor = document.querySelector ("#send");
+pegarvalor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const numerocartao = document.querySelector("#numerocartao");
+    const value = numerocartao.value;
+    console.log(value);
+});
