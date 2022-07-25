e9c65fc5ba08661866bfdc907a25f3ec354d6106
raquelthabata <r.thabata05@gmail.com>
2022-07-24 22:51:57 -0300

    estilizaçoes iniciais

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const botao= document.getElementById("validador")
+botao.addEventListener("click",function(event){
+    console.log(event)
+})
