42a8175ac6441bc98f9f2eb76e36d30f34747d45
raquelthabata <r.thabata05@gmail.com>
2022-08-02 23:37:55 -0300

    implementaçao de for e array reverse no js

--- src/index.js
+++ src/index.js
@@ -11,13 +11,26 @@ function typeWriter(e) {
     });
 }
 const palavras = document.querySelector('p')
-typeWriter(palavras)
+typeWriter(palavras) 
 
 const pegarvalor = document.querySelector ("#send");
 pegarvalor.addEventListener("click", function(e) {
     e.preventDefault();
     const numerocartao = document.querySelector("#numerocartao");
-    const value = numerocartao.value;
-    console.log(value);
-});
+    const value = numerocartao.value; //"12345"
+    let arrayNumber = value.split('').map(Number); //[1,2,3,4,5]
+    arrayNumber.reverse(); //[5,4,3,2,1]
 
+    for(let a = 1; a <  arrayNumber.length;){ //começa com a na posição 1; a tem que ser menor que o tamanho do array (condição); 
+        let valueNumberPosition = arrayNumber[a] * 2; //Criei uma variavel Multiplicar por 2 o valor da posição do array arrayNumber
+        arrayNumber[a] = valueNumberPosition; //substitui o valor da posiçao pelo resultado da valueNumberPosition
+        a = a + 2; //incrementação
+    }
+
+    console.log(arrayNumber);
+
+    //TODO: Entrar novamente no array e verificar todos os valores maiores que 9.
+    //for (para entrar na posição) if(identificar os valores maiores que 9)
+    
+
+});
