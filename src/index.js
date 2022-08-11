3fefbe1ced14c2a8450289757ee6e54b80b38681
raquelthabata <r.thabata05@gmail.com>
2022-08-11 12:38:24 -0300

    removendo comentários e editando redme

--- src/index.js
+++ src/index.js
@@ -3,41 +3,33 @@ import validator from './validator.js';
 console.log(validator);
 
 
-function typeWriter(e) {
+function typeWriter(e) 
+{
     const textoArray = e.innerHTML.split('')
     e.innerHTML = ''
-    textoArray.forEach((letra, i) => {
-        setTimeout(() => e.innerHTML += letra, 75 * i)
-    });
+    textoArray.forEach((letra, i) => 
+    {setTimeout(() => e.innerHTML += letra, 75 * i)});
 }
 const palavras = document.querySelector('h1')
 typeWriter(palavras) 
 
 const pegarvalor = document.querySelector ("#send");
-
 pegarvalor.addEventListener("click", function(e) {
     e.preventDefault();
     const numerocartao = document.querySelector("#numerocartao");
-    const value = numerocartao.value; //"12345"
-    const isValid = validator.isValueNotEmpty(value); //
+    const value = numerocartao.value;
+    const isValid = validator.isValueNotEmpty(value);
 
     if(isValid){
-        let arrayNumber = value.split('').map(Number); //[1,2,3,4,5]
-        arrayNumber.reverse(); //[5,4,3,2,1]
+        let arrayNumber = value.split('').map(Number);
+        arrayNumber.reverse();
+        for(let a = 1; a <  arrayNumber.length;a = a + 2)
+        {let valueNumberPosition = arrayNumber[a] * 2;
+        if ( valueNumberPosition >9) {
+                valueNumberPosition = valueNumberPosition - 9 }
+                 arrayNumber[a] = valueNumberPosition;}
 
-
-
-        for(let a = 1; a <  arrayNumber.length;a = a + 2){ //começa com a na posição 1; a tem que ser menor que o tamanho do array (condição); 
-            let valueNumberPosition = arrayNumber[a] * 2; //Criei uma variavel Multiplicar por 2 o valor da posição do array arrayNumber
-            if ( valueNumberPosition >9) {
-                valueNumberPosition = valueNumberPosition - 9
-            }
-            arrayNumber[a] = valueNumberPosition; //substitui o valor da posiçao pelo resultado da valueNumberPosition
-        
-        }
-        console.log(arrayNumber);
-
-        var soma= arrayNumber.reduce((soma,arrayNumber)=> soma+arrayNumber, 0);//soma de todos os números do array
+        var soma= arrayNumber.reduce((soma,arrayNumber)=> soma+arrayNumber, 0);
 
         let maskNumberForChar = validator.maskify(arrayNumber).toString();
         let semVirgula = maskNumberForChar.replaceAll(",", "");
